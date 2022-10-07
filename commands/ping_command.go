4d4d7b1b4896ddaa9e9cce3543e699f7892dba64
Kstoums <kstoums@gmail.com>
2022-10-07 15:45:15 +0200

    Added 90% of ping command content

--- commands/ping_command.go
+++ commands/ping_command.go
@@ -1,7 +1,15 @@
 package commands
 
 import (
+	"2SpiiCrasher/messages"
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/tatsushid/go-fastping"
+	"log"
+	"net"
+	"strconv"
+	"sync"
+	"time"
 )
 
 var pingCommand = &cobra.Command{
@@ -11,7 +19,50 @@ var pingCommand = &cobra.Command{
 	Example: "2spiicrasher ping 100 play.myserver.net",
 	Args:    cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
-		// WORK
+		mountPing, err := strconv.ParseInt(args[0], 6, 12)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if mountPing > 20000 || mountPing < 1 {
+			log.Fatal(messages.MountPingTooHigh)
+		}
+
+		serverAddress := args[1]
+
+		ping := fastping.NewPinger()
+		resolveAddress, err := net.ResolveIPAddr("ip4:icmp", serverAddress)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		ping.AddIPAddr(resolveAddress)
+
+		waitGroup := sync.WaitGroup{}
+		waitGroup.Add(int(mountPing))
+
+		for i := 0; i < int(mountPing); i++ {
+			go func() {
+				ping.OnRecv = func(ipAddr *net.IPAddr, duration time.Duration) {
+					fmt.Println(fmt.Sprintf("IP Address: %s receive, RTT: %v", ipAddr.String(), duration))
+				}
+
+				ping.OnIdle = func() {
+					fmt.Println(messages.PositiveAnswer)
+				}
+
+				err = ping.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				waitGroup.Done()
+			}()
+		}
+
+		waitGroup.Wait()
+		return
+
 	},
 }
 
--- messages/messages.go
+++ messages/messages.go
@@ -1,5 +1,6 @@
 package messages
 
-var MountBotTooHigh = "Error: The amount set is invalid. (Min 1 | Max 1000)"
+var MountBotTooHigh = "Error: The amount set is invalid. (Min 1 | Max 20.000)"
+var MountPingTooHigh = "Error: The amount set is invalid. (Min 1 | Max 20.000)"
 var PingRequestWaitingResponse = "Ping request sent. Waiting for an answer"
 var PositiveAnswer = "The ping request was answered successfully."
